38a64c2020ee98fd81e9318c658bea3721d9d698
Toshiaki Maki <tmaki@pivotal.io>
2019-03-02 18:32:05 +0900

    Bump to zipkin-dependencies 2.0.6

--- add-blobs.sh
+++ add-blobs.sh
@@ -9,12 +9,12 @@ cd .downloads
 
 
 ZIPKIN_VERSION=2.12.3
-ZIPKIN_DEPENDENCIES_VERSION=2.0.4
+ZIPKIN_DEPENDENCIES_VERSION=2.0.6
 
-if [ ! -f ${DIR}/blobs/zipkin/zipkin-server-${ZIPKIN_VERSION}-exec.jar ];then
-    curl -L -O -J https://repo1.maven.org/maven2/io/zipkin/java/zipkin-server/${ZIPKIN_VERSION}/zipkin-server-${ZIPKIN_VERSION}-exec.jar
-    bosh add-blob --dir=${DIR} zipkin-server-${ZIPKIN_VERSION}-exec.jar zipkin/zipkin-server-${ZIPKIN_VERSION}-exec.jar
-fi
+# if [ ! -f ${DIR}/blobs/zipkin/zipkin-server-${ZIPKIN_VERSION}-exec.jar ];then
+#     curl -L -O -J https://repo1.maven.org/maven2/io/zipkin/java/zipkin-server/${ZIPKIN_VERSION}/zipkin-server-${ZIPKIN_VERSION}-exec.jar
+#     bosh add-blob --dir=${DIR} zipkin-server-${ZIPKIN_VERSION}-exec.jar zipkin/zipkin-server-${ZIPKIN_VERSION}-exec.jar
+# fi
 
 if [ ! -f ${DIR}/blobs/zipkin-dependencies/zipkin-dependencies-${ZIPKIN_DEPENDENCIES_VERSION}.jar ];then
     curl -L -O -J https://repo1.maven.org/maven2/io/zipkin/dependencies/zipkin-dependencies/${ZIPKIN_DEPENDENCIES_VERSION}/zipkin-dependencies-${ZIPKIN_DEPENDENCIES_VERSION}.jar
